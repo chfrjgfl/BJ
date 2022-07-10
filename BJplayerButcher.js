47fc899bf6c38aa5d22f7e88684ebad5decb7071
chfrjgfl <104519853+chfrjgfl@users.noreply.github.com>
2022-07-10 18:50:18 +0300

    Add files via upload

---
+++ BJplayerButcher.js
@@ -0,0 +1,61 @@
+const threads = require("worker_threads");
+const myName = 'Butcher';
+const spares0 = [8, 8, 8, 4, 4, 4 ];
+let spares = [8, 8, 8, 4, 4, 4 ];        //qty of cards with value = 1,2,3,4,5,6,7
+let probEdge;
+const hand = [];
+let countCards = false;
+let score = 0;
+let restOfDeck = 52;
+let msg;
+let playerEdge;
+
+threads.parentPort.on("message", t => { 
+    
+    switch (t.cmd) {
+        case 'i':   countCards = t.body.c;
+                    playerEdge = t.body.e || 17;
+                    probEdge = t.body.p || 0.5;
+                    break; 
+
+        case 'n':   threads.parentPort.postMessage({cmd: 'd',hand: hand, score: score});
+                    hand.splice(0);
+                    score = 0;                                    
+                    break;
+
+        case 'c':   if (countCards) {
+                        restOfDeck --;
+                        if (t.body.val < 8) spares[t.body.val-2] --;
+                    }
+                    hand.push(t.body);
+                    score += t.body.val;                    
+                    threads.parentPort.postMessage({cmd: 'g', body: t.body, score: score});
+
+                    if (score == 21 ||(hand.length == 2 && score == 22)) {       //2 aces
+                        msg = 'w';                          //Won
+                    } else if (score > 21) {               
+                        msg = 'l';                          //Lost
+                    } else if (score == 20) {
+                        msg = 's';                          //Stop at 20
+                    } else if (score > playerEdge) {
+                        let prob = countCards? 
+                            spares.slice(0, 20 - score).reduce((a,b) => a + b, 0) / restOfDeck: 0;
+                        msg = prob < probEdge ? 's': 'm';           // Stop: More
+                    } else msg = 'm' ;
+                    threads.parentPort.postMessage({cmd: msg, hand: hand, score: score});
+                    break;
+
+        case 'd':   if (t.body.name != myName) {
+                        restOfDeck -= t.body.hand.length;
+                        for (let s of t.body.hand) {
+                            if (s.val < 8) spares[s.val-2] --; 
+                        }
+                    }
+                    break;
+
+        case 'a':   restOfDeck += 52;
+                    spares = spares.map((value, index) => { return value + spares0[index] });
+                    break;            
+    }
+
+});
---
+++ BlackJack.js
@@ -0,0 +1,159 @@
+const threads = require("worker_threads");
+
+const suits  = ['spades', 'clubs', 'diamonds', 'hearts'];
+const names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const vals = [2,3,4,5,6,7,8,9,10,2,3,4,11];         //74 per 1 suit,  296 per deck
+const dealerLimit = 17;                        //most often dealer must hit at 17
+
+const stats = {
+    totalScore: 0,
+    totalRounds: 0,
+    butcherWon21: 0,
+    dealerWon21: 0,
+    butcherHasMore: 0,
+    dealerHasMore: 0,
+    butcherOver: 0,
+    dealerOver:0,
+    dealerWonByEqual: 0,
+    cardsPlayedButcher: 1,
+    cardsPlayedDealer: 0
+};
+
+const butcher = {
+    w:new threads.Worker("./BJplayerButcher.js"),
+    score: 0,                                       // for future use
+    options:  {c: true, e: 13, p: 0.6}              // see below
+};
+
+let numberOfDecks = 10;
+let deckValue = 296;
+let deck = addNewDeck([]);
+
+/*      Commands to player:
+'n' - new round   (no body)
+'c' - card is dealt {card}
+'i' - init options  {   name,
+                        c: countCards, 
+                        e: playerEdge,             <---------
+                        p: probabilityEdge}
+'d' - dropping a hand {name, hand: [array of cards]}
+'a' - add a deck   (no body)
+                                        
+*/
+butcher.w.postMessage({cmd: 'i', body: butcher.options});   //n: numberOfDecks,   //  
+butcher.w.postMessage({cmd: 'c', body: deck.pop()});
+
+butcher.w.on("message", t =>  {
+    console.log('Butcher says:', JSON.stringify(t));
+    let mode = t.cmd;
+
+    if (mode === 's') {   
+        let dealerScore = dealSelf(t.score);  
+
+        if (dealerScore <= 21 && dealerScore >= t.score) {
+            mode = 'l';
+            if (dealerScore == 21) stats.dealerWon21 ++;
+            else if (dealerScore == t.score) stats.dealerWonByEqual ++;
+            else stats.dealerHasMore ++;
+        } else {
+            mode = 'w';
+            if (dealerScore > 21) stats.dealerOver ++;
+            else stats.butcherHasMore ++;
+        }  
+    }
+
+    if (mode === 'l') {
+        stats.totalScore++;
+        console.log ('Dealer won!',  `Total score ${stats.totalScore}`);
+        if (t.score > 21) stats.butcherOver ++;
+    }
+
+    if (mode === 'w') {
+        stats.totalScore--;
+        console.log ('Player won!',  `Total score ${stats.totalScore}`);
+        if (t.score >= 21) stats.butcherWon21 ++;
+    }
+
+    if (['l', 'w'].includes(mode)) {
+        deckValue -= t.score;
+        stats.totalRounds ++;
+
+        checkDeck(21);
+                
+        butcher.w.postMessage({cmd: 'n'});        
+        mode = 'm';    
+    }
+
+    if (mode === 'm') { 
+        stats.cardsPlayedButcher ++;
+        butcher.w.postMessage({cmd: 'c', body: deck.pop()});
+    }  
+});
+
+butcher.w.on("exit", () => process.exit(2));
+
+console.log('-----');
+
+
+
+function dealSelf(offset) {
+    let score = 0;
+    let card = {};
+    const hand = [];
+
+    checkDeck(dealerLimit + offset + 1);
+
+    while (score <= dealerLimit) {
+        card = deck.pop();
+        hand.push(card);        
+        score += card.val;
+        console.log(`Dealer's got ${card.name} of ${card.suit}; total ${score}; DV ${deckValue}`);
+    }
+    stats.cardsPlayedDealer += hand.length;
+    butcher.w.postMessage({cmd: 'd', body: {name: 'Dealer', hand: hand}});
+
+    deckValue -= score;
+    if (hand.length == 2 && score == 22) return 21;   // 2 aces
+    return score;
+}
+
+function printStat() {
+    console.log ('------------'); 
+    console.log (JSON.stringify(stats));
+    process.exit(20);
+}
+
+function addNewDeck(deck) {
+    const newDeck = [];
+    for (let s of suits) {
+        for (let n of names) {
+            let card = {};
+            card.suit = s;
+            card.name = n;
+            card.val = vals[names.indexOf(n)];        
+            let pos = Math.floor(52*Math.random());
+            while (newDeck[pos]) {
+                pos = (pos+1) % 52;
+            }
+            newDeck[pos] = card;        
+        }
+    }
+    return newDeck.concat(deck);
+}
+
+function checkDeck(edge) {
+    if (deckValue < edge) {  
+        if (numberOfDecks > 1) {
+            deck = addNewDeck(deck);
+            deckValue += 296;
+            numberOfDecks --;
+            console.log ('Deck added');
+            butcher.w.postMessage({cmd: 'a'});
+        } else {
+            console.log (`Game over! Total score ${stats.totalScore}`);
+            butcher.w.terminate();
+            printStat();
+            
+        }
+    } 
+}
